bf20a3542b714583ed173b4cde3050db7b7c6c44
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-07-11 10:59:09 -0500

    Reorganize imports in main.jsx for better readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import { queryClient } from "./lib/queryClient.js";
+
+import "./index.css";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
