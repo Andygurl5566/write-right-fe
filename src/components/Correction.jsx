318cf86964625fd53d2734b426d337b5a2211999
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-07-14 12:55:10 -0500

    feat: enhance Correction and CorrectionTooltip components with styling and functionality

--- src/components/Correction.jsx
+++ src/components/Correction.jsx
@@ -1,9 +1,28 @@
-import CorrectionTooltip from "./CorrectionTooltip"
+import "./Correction.css";
+import CorrectionTooltip from "./CorrectionTooltip";
 
+// returns a single correction
+function Correction({ mistake }) {
 
-//returns a single correction
-function Correction(original, correction, explanation) {
-  return <></>;
+    console.log("Correction:", mistake);
+
+    return (
+        <span className="correction-wrapper">
+
+            <span className="incorrect-text">
+                {mistake.original}
+            </span>
+
+            <span className="corrected-text">
+                {mistake.corrected_text}
+            </span>
+
+            <CorrectionTooltip>
+                {mistake.explanation}
+            </CorrectionTooltip>
+
+        </span>
+    );
 }
 
 export default Correction;
--- src/components/CorrectionTooltip.jsx
+++ src/components/CorrectionTooltip.jsx
@@ -1,5 +1,13 @@
-function CorrectionTooltip() {
-  return <></>;
+import "./CorrectionToolTip.css";
+
+// This component is used to display a tooltip with an explanation of the correction and a button to create a flashcard.
+function CorrectionTooltip({ children }) {
+  return (
+    <div className="tooltip-container">
+      <div className="tooltip-content">{children}</div>
+      <button>Create Flashcard</button>
+    </div>
+  );
 }
 
 export default CorrectionTooltip;
