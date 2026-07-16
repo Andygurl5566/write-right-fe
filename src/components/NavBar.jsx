726748ed8aba5153ae3ee2f99784cc7af66473cd
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-07-16 02:07:07 -0500

    Refactor z-index values in Correction and NavBar components for consistency

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import "./Navbar.css";
 import TranslateOutlinedIcon from "@mui/icons-material/TranslateOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import { Stack } from "@mui/material";
 
 function Navbar() {
   return (
@@ -10,14 +11,17 @@ function Navbar() {
         <span className="logo-icon">✍️</span>
         <span className="logo-text">WriteRight</span>
       </div>
+
       <span>Write</span>
       <span>History</span>
       <span>Flashcards</span>
 
       <div className="navbar-user">
-        <AccountCircleOutlinedIcon fontSize="large" />
+        <Stack direction="row" spacing={2}>
+          <AccountCircleOutlinedIcon fontSize="large" />
 
-        {/* <span>DAD</span> */}
+          {/* <span>DAD</span> */}
+        </Stack>
       </div>
     </nav>
   );
